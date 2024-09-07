634322d49a1ba27365456f2f12d0be8bba44452d
Satyam Singh <mesatyamsingh1999@gmail.com>
2024-09-07 23:57:30 +0530

    basic setup

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+
+1. > npm i redux react-redux @reduxjs/toolkit moment 
+2.  npm i antd
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from 'antd';
+
+
+function App() {
+  
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
